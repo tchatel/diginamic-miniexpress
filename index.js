ef41db84c8de6a1764b5caf178b26b31c98f89c3
Thierry Chatel <tchatel@gmail.com>
2020-06-18 10:26:18 +0200

    init

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const miniExpress = require('./mini-express');
 
 const app = miniExpress();
@@ -6,6 +8,33 @@ app.use((req, res, next) => {
     console.log('Time:', Date.now());
     next();
 });
+
+const favicon = (iconPath) => {
+  return (req, res, next) => {
+    if (req.method.toLowerCase() === 'get'
+        && req.url === '/favicon.ico') {
+      res.writeHead(200, {'Content-type': 'image/x-icon'});
+      const iconReadable = fs.createReadStream(iconPath);
+      iconReadable.pipe(res)
+        // .on('finish') {
+        //   next();
+        // }
+    } else {
+      next();
+    }
+  };
+};
+// const faviconMiddleware = favicon(__dirname + '/favicon.ico');
+// app.use(faviconMiddleware);
+app.use(favicon(__dirname + '/favicon.ico'));
+
+
+
+// app.get('/favicon.ico', (req, res) => {
+//   res.writeHead(200, {'Content-type': 'image/x-icon'});
+//   const iconReadable = fs.createReadStream(__dirname + '/favicon.ico');
+//   iconReadable.pipe(res)
+// });
   
 app.get('/', (req, res) => {
     // res.send('Hello World!');
